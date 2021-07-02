75f58b2edcfe64f6d618fd0d52d6ce2046023b35
La2New <artezianplanet@yandex.ru>
2021-07-02 12:02:07 +0300

    add logger

--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,9 +1,9 @@
 import {accountReducer} from "../reducers/accountReducer";
 import {applyMiddleware, createStore} from "redux";
-import {loggerEnthancer} from "../enthancers/loggerEnthancer";
+import {logger} from "redux-logger/src";
 
 export const initialState = {
     balance: 0,
     quote: 'Winter is coming...'
 }
-export const store = createStore(accountReducer, initialState, applyMiddleware(loggerEnthancer));
+export const store = createStore(accountReducer, initialState, applyMiddleware(logger));
